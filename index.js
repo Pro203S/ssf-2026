b8ad3044c8cf9a8ad5db2b369022f1b0b8d61b0e
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-07-09 17:47:41 +0900

    아이귀찮아

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("canvas");
+
+    canvas.setAttribute("style", `background-image: url("/assets/images/backgrounds/main.png")`);
+
+    const pot1 = document.getElementById("pot1");
+    const pot2 = document.getElementById("pot2");
+});
